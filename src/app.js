a543685acc826649528261cf10ebf10193271d8e
PieroMastro <pier.andres@gmail.com>
2022-09-04 01:56:19 +0000

    Initial setup

--- src/app.js
+++ src/app.js
@@ -7,5 +7,8 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let excuse = "mi perro se lo comio";
+  let excuseP = document.querySelector("#excuse");
+  excuseP.textContent = excuse;
   console.log("Hello Rigo from the console!");
 };
