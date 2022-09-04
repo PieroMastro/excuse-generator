78456fa0971892ebec2047bbcfa442907be06841
PieroMastro <pier.andres@gmail.com>
2022-09-04 04:28:52 +0000

    Added arrays & funtion, minor style changes

--- src/app.js
+++ src/app.js
@@ -7,8 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let excuse = "mi perro se lo comio";
-  let excuseP = document.querySelector("#excuse");
-  excuseP.textContent = excuse;
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird", "A porcupine"];
+
+  let action = ["ate", "peed", "crushed", "broke", "hid"];
+
+  let what = ["my homework", "the keys", "the car", "my phone", "the TV"];
+
+  let when = [
+    "before the class",
+    "when I was outside",
+    "watching a movie",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuse = document.querySelector("#excuse");
+
+  excuse.textContent =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)] +
+    "!";
+
+  console.log("My Excuse Generator!");
 };
